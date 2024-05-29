6bcce1170c59a8196904a4dc09725d0c394a6568
Christian Pschorr <pschorr.christian@gmail.com>
2024-05-29 10:42:03 +0200

    Übernahme Tabellenkonfiguration für Categories

--- Configuration/TCA/Overrides/sys_category.php
+++ Configuration/TCA/Overrides/sys_category.php
@@ -1,34 +1,38 @@
 <?php
-defined('TYPO3_MODE') || die();
 
-// ---------------------------------------------------------------------------------------------------------------------
-// Neue Spalten
-$tmpSysCategoryColumns = [
-	'tx_contact_zip_regex' => [
-		'exclude' => 1,
-		'label' => 'LLL:EXT:contact/Resources/Private/Language/locallang_db.xlf:tx_contact_domain_model_country.tx_contact_zip_regex',
-		'config' => [
-			'type' => 'input',
-			'size' => 30,
-			'eval' => 'trim'
-		],
-	],
-	'tx_contact_country_description' => [
-		'exclude' => 1,
-		'label' => 'LLL:EXT:contact/Resources/Private/Language/locallang_db.xlf:tx_contact_domain_model_country.tx_contact_country_description',
-		'config' => [
-			'type' => 'text',
-			'enableRichtext' => true,
-			'richtextConfiguration' => 'xoMinimal',
-			'fieldControl' => [
-				'fullScreenRichtext' => [
-					'disabled' => false,
-				],
+(function() {
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Weitere Felder in Categories
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_category', [
+		'tx_contact_zip_regex' => [
+			'exclude' => 1,
+			'label' => 'LLL:EXT:contact/Resources/Private/Language/locallang_db.xlf:tx_contact_domain_model_country.tx_contact_zip_regex',
+			'config' => [
+				'type' => 'input',
+				'size' => 30,
+				'eval' => 'trim'
 			],
-			'eval' => 'trim',
 		],
-	],
-];
+		'tx_contact_country_description' => [
+			'exclude' => 1,
+			'label' => 'LLL:EXT:contact/Resources/Private/Language/locallang_db.xlf:tx_contact_domain_model_country.tx_contact_country_description',
+			'config' => [
+				'type' => 'text',
+				'enableRichtext' => true,
+				'richtextConfiguration' => 'ps14Minimal',
+				'fieldControl' => [
+					'fullScreenRichtext' => [
+						'disabled' => false,
+					],
+				],
+				'eval' => 'trim',
+			],
+		],
+	]);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_category', $tmpSysCategoryColumns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('sys_category', '--linebreak--, tx_contact_zip_regex, tx_contact_country_description,', '', 'after:description');
+	// -------------------------------------------------------------------------------------------------------------------
+	// Neue Felder einsortieren
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('sys_category', ', --linebreak--, tx_contact_zip_regex', '', 'after:title');
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('sys_category', ', --linebreak--, tx_contact_country_description', '', 'after:parent');
+})();
