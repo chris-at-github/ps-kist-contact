b3a2f8d7f7043abdcb5661861c550c36cffde992
Christian Pschorr <pschorr.christian@gmail.com>
2024-05-29 12:22:22 +0200

    Migration der Tabellendefinition für tt_content

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,7 +1,5 @@
 <?php
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['contact_search'] = 'bodytext';
-$GLOBALS['TCA']['tt_content']['types']['list']['columnsOverrides']['bodytext']['config'] = [
-	'enableRichtext' => true,
-	'richtextConfiguration' => 'xoDefault',
-];
+(function() {
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['contact_search'] = 'bodytext';
+})();
